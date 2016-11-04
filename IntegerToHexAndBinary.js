6c055324e7015f02f6a77b75e3af2917558d7525
PlamenHP <p.petrov.mail@gmail.com>
2016-11-04 23:56:43 +0000

    js task finished

---
+++ IntegerToHexAndBinary.js
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 04/11/2016.
+ */
+function f([number]) {
+
+    let num = parseInt(number);
+    console.log(num.toString(16).toUpperCase());
+    console.log(num.toString(2).toUpperCase())
+}
+
+f(10);
---
+++ Web Technologies and Language Specifics - Exercise/Employee Data.js
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 29/10/2016.
+ */
+function ([fname,lname,age,gender,PID,UEN]) {
+
+    console.log("First name: "+ fname);
+    console.log("Last name: "+ lname);
+    console.log("Age: "+ age);
+    console.log("Gender: "+ gender);
+    console.log("Personal ID: "+ PID);
+    console.log("Unique Employee number: "+ UEN);
+}
