1f548fa205acd58251c7ccc43a32b8e212c0045f
PlamenHP <p.petrov.mail@gmail.com>
2016-11-20 12:56:48 +0000

    user consistency update

--- SoftUniBlog/controllers/user.js
+++ SoftUniBlog/controllers/user.js
@@ -7,7 +7,7 @@ module.exports = {
         res.render('user/register');
     },
 
-    registerPost:(req, res) => {
+    registerPost: (req, res) => {
         let registerArgs = req.body;
 
         User.findOne({email: registerArgs.email}).then(user => {
@@ -38,25 +38,18 @@ module.exports = {
 
                     userObject.roles = roles;
                     User.create(userObject).then(user => {
-                        role.users.push(user);
-                        role.save(err => {
-                            if(err) {
+                        user.prepareInsert();
+
+                        req.logIn(user, (err) => {
+                            if (err) {
                                 registerArgs.error = err.message;
                                 res.render('user/register', registerArgs);
+                                return;
                             }
-                            else {
-                                req.logIn(user, (err) => {
-                                    if (err) {
-                                        registerArgs.error = err.message;
-                                        res.render('user/register', registerArgs);
-                                        return;
-                                    }
 
-                                    res.redirect('/');
-                                })
-                            }
-                        });
-                    });
+                            res.redirect('/');
+                        })
+                    })
                 });
             }
         })
@@ -69,7 +62,7 @@ module.exports = {
     loginPost: (req, res) => {
         let loginArgs = req.body;
         User.findOne({email: loginArgs.email}).then(user => {
-            if (!user ||!user.authenticate(loginArgs.password)) {
+            if (!user || !user.authenticate(loginArgs.password)) {
                 let errorMsg = 'Either username or password is invalid!';
                 loginArgs.error = errorMsg;
                 res.render('user/login', loginArgs);
@@ -83,7 +76,7 @@ module.exports = {
                 }
 
                 let returnUrl = '/';
-                if(req.session.returnUrl) {
+                if (req.session.returnUrl) {
                     returnUrl = req.session.returnUrl;
                     delete req.session.returnUrl;
                 }
